2eadd9fd16efad33b7ed7521347942f40155fc42
xosnrdev <successxodev@gmail.com>
2024-11-11 04:31:51 -0500

    quick fix

---
+++ elmio-js/src/effects/console.ts
@@ -0,0 +1,29 @@
+import { IConsole } from "../browser/console";
+import { Domain, Logger } from "../logger";
+import { ConsoleEffect, Log } from "../rust/types";
+
+export class ConsoleEffectHandler {
+    constructor(
+        private readonly console: IConsole<string>,
+        private readonly logger: Logger<object>,
+    ) {}
+
+    public handle(effect: ConsoleEffect): Promise<void> {
+        switch (effect.type) {
+            case "log":
+                this.log(effect.config);
+            default:
+                this.logger.warn({
+                    domain: Domain.Console,
+                    message: `Unknown effect type: ${effect.type}`,
+                    context: { type: effect.type },
+                });
+        }
+
+        return Promise.resolve();
+    }
+
+    private log(config: Log): void {
+        this.console.log(config.message);
+    }
+}
