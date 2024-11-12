8a2182afb321a6c77ed1021b3e4a00c97e2b47c8
xosnrdev <successxodev@gmail.com>
2024-11-12 10:49:35 -0500

    Added main entrypoint

---
+++ elmio-js/src/index.ts
@@ -0,0 +1,7 @@
+import { Browser, RealBrowser } from "./browser";
+import { BrowserWindow, Window } from "./browser/window";
+import { Config, Elmio } from "./elmio";
+import { rustEnum } from "./rust/enum";
+import { Page } from "./rust/types";
+
+export { Elmio, Config, Page, Browser, RealBrowser, Window, BrowserWindow, rustEnum };
