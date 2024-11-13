eb3c270d7e7df2a3995d4dd58fa267ca81177d01
xosnrdev <successxodev@gmail.com>
2024-11-13 14:08:49 -0500

    prepare for publish to npm

---
+++ elmio-js/publish.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+main() {
+    local version
+    version=$(get_version "$1")
+    echo "New version: $version"
+
+    check_git_clean
+
+    pnpm build
+
+    set_version "$version"
+
+    commit_and_push
+
+    npm publish --access public --tag latest
+}
+
+get_version() {
+    local version="$1"
+    if [[ -z "$version" ]]; then
+        jq -r '.version' package.json
+    else
+        echo "$version"
+    fi
+}
+
+check_git_clean() {
+    if [[ -n $(git status --porcelain) ]]; then
+        echo "Uncommitted files detected. Aborting release."
+        exit 1
+    fi
+}
+
+set_version() {
+    local version="$1"
+    jq --arg version "$version" '.version = $version' package.json >package.json.tmp
+    mv package.json.tmp package.json
+}
+
+commit_and_push() {
+    git add .
+    git commit -m "Publishing package"
+    git push
+}
+
+main "$@"
---
+++ elmio-js/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/index.ts"],
+    clean: true,
+    minify: true,
+    dts: true,
+    format: ["esm"],
+    treeshake: true,
+});
