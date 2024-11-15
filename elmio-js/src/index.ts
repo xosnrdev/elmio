1ceb885a7574d32ef6b27e329ddfab6af3b334cf
xosnrdev <successxodev@gmail.com>
2024-11-15 10:58:37 -0500

    quick patch

--- elmio-js/src/index.ts
+++ elmio-js/src/index.ts
@@ -1,7 +1,18 @@
 import { Browser, RealBrowser } from "./browser";
 import { BrowserWindow, Window } from "./browser/window";
 import { Config, Elmio } from "./elmio";
+import { defaultDebugConfig } from "./logger";
 import { rustEnum } from "./rust/enum";
 import { Page } from "./rust/types";
 
-export { Elmio, Config, Page, Browser, RealBrowser, Window, BrowserWindow, rustEnum };
+export {
+    Elmio,
+    Config,
+    Page,
+    Browser,
+    RealBrowser,
+    Window,
+    BrowserWindow,
+    rustEnum,
+    defaultDebugConfig,
+};
