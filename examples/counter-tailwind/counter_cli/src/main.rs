a594df8b0a51ede17a4175f12a6a893d7b7e2501
xosnrdev <successxodev@gmail.com>
2024-11-20 10:36:14 -0500

    chore: added counter app example

---
+++ examples/counter-tailwind/counter_cli/src/main.rs
@@ -0,0 +1,33 @@
+use std::{env, process};
+
+use counter_core::home_page;
+use elmio_core::page::Page;
+use url::Url;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: counter_cli <command>");
+        process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "home_page" => {
+            let page = home_page::HomePage {
+                current_url: Url::parse("http://localhost/").unwrap(),
+            };
+            print_html(page);
+        }
+        _ => {
+            eprintln!("Invalid command: {:?}", args);
+            process::exit(1);
+        }
+    }
+}
+
+fn print_html<Model, Msg, Markup>(page: impl Page<Model, Msg, Markup>) {
+    let (model, _effects) = page.init().expect("Failed to init page");
+    let markup = page.view(&model);
+    println!("{}", page.render_page(markup));
+}
